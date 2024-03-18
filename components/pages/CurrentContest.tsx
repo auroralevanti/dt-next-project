0b869f3a62b8266c68cabbae73a0e86afe2b6cef
auroralevanti <auroralevanti@gmail.com>
2024-03-18 00:42:38 -0400

    act

--- components/pages/CurrentContest.tsx
+++ components/pages/CurrentContest.tsx
@@ -8,8 +8,8 @@ import { useRouter } from 'next/router';
 
 
 interface Input {
-    idUserDiscord: string,
-    nameUserDiscord: string
+    discordId: string,
+    discordName: string
 }
 
 
@@ -19,31 +19,33 @@ export const CurrentContest = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm<Input>();
 
-    const onSubmit: SubmitHandler<Input> = async({ idUserDiscord, nameUserDiscord }: Input) => {
+    const onSubmit: SubmitHandler<Input> = async ({ discordId, discordName }: Input) => {
+
 
         try {
-            const backToSave = await fetch('/api/contestants/getInContest', {
+            const backToSave = await fetch('/api/contestants/getInToContest', {
                 method: 'POST',
-                headers:{
+                headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ idUserDiscord, nameUserDiscord })
+                body: JSON.stringify({ discordId, discordName })
             })
 
             const response = await backToSave.json();
+            console.log(response)
 
-            if(response.Objeto.transaction.resultado === true){
-                router.replace('/concursante/ingreso-exitoso')
+            if (response.Objeto.transaction.resultado === true) {
+                router.push('concursante/ingreso-exitoso')
             };
 
-            if(response.Objeto.transaction.resultado === false){
-                router.replace('/concursante/ingreso-fallido')
+            if (response.Objeto.transaction.resultado === false) {
+                router.push('concursante/ingreso-fallido')
             };
-            
+
         } catch (error) {
 
-            router.replace('/concursante/ingreso-fallido');
-            
+            router.replace('concursante/ingreso-fallido');
+
         }
 
     }
@@ -54,68 +56,68 @@ export const CurrentContest = () => {
             <Grid container display='flex' justifyContent='center' padding={10}>
                 <Grid item xs={12} md={12} padding={1} display='flex' justifyItems='left' paddingLeft={5}>
                     <Typography variant="body1" textAlign='center' fontWeight={300} fontSize='24px' color='white' >¡El desafío comienza!</Typography>
-                   
+
                 </Grid>
 
                 <Grid item xs={12} md={12} padding={1} display='flex' justifyItems='left' paddingLeft={10}>
                     <Typography variant="body1" textAlign='center' fontWeight={600} fontSize='40px' color='white' >CodeQuest 2024</Typography>
                 </Grid>
 
-                
-                    <Card sx={{ backgroundColor:'#e8d2ff', paddingRight: 2, paddingLeft: 2, paddingTop: 4, paddingBottom: 4 }}>
-                       
-                        <Typography variant='body1' fontWeight={300} fontSize='20px'paddingBottom={1}>Su misión es desarrollar <b>una webapp</b> usando cualquier tecnología usada en los cursos de Fernando.</Typography>
-                        <Typography variant='body1' fontWeight={300} fontSize='20px' paddingBottom={1}>Siguiendo las reglas que se encuentran en el Reglamento del concurso <b>descarga aquì.</b>  </Typography>
-                        <Typography variant='body1' fontWeight={300} fontSize='20px' paddingBottom={5}>Para formar parte solo debes llenar los datos requeridos y seguir las reglas. </Typography>
 
-                        <form onSubmit={handleSubmit(onSubmit)}>
-       
-                            <TextField
-                                fullWidth
-                                autoFocus
-                                variant='filled'
-                                label='ID de Discord'
-                                id='idUserDiscord'
-                                {...register('idUserDiscord', { required: true })}
-                                error={!!errors?.idUserDiscord}
-                                sx={{ paddingBottom: 5 }}
-                                >
+                <Card sx={{ backgroundColor: '#e8d2ff', paddingRight: 2, paddingLeft: 2, paddingTop: 4, paddingBottom: 4 }}>
 
-                             </TextField>
-                            <br />
-           
-                            <TextField
+                    <Typography variant='body1' fontWeight={300} fontSize='20px' paddingBottom={1}>Su misión es desarrollar <b>una webapp</b> usando cualquier tecnología usada en los cursos de Fernando.</Typography>
+                    <Typography variant='body1' fontWeight={300} fontSize='20px' paddingBottom={1}>Siguiendo las reglas que se encuentran en el Reglamento del concurso <b>descarga aquì.</b>  </Typography>
+                    <Typography variant='body1' fontWeight={300} fontSize='20px' paddingBottom={5}>Para formar parte solo debes llenar los datos requeridos y seguir las reglas. </Typography>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <TextField
+                            fullWidth
+                            autoFocus
+                            variant='filled'
+                            label='ID de Discord'
+                            id='discordId'
+                            {...register('discordId', { required: true })}
+                            error={!!errors?.discordId}
+                            sx={{ paddingBottom: 5 }}
+                        >
+
+                        </TextField>
+                        <br />
+
+                        <TextField
                             fullWidth
                             variant='filled'
                             label='Nombre Usuario en Discord'
-                            id='nameUserDiscord'
-                            {...register('nameUserDiscord', { required: true })}
-                            error={!!errors?.nameUserDiscord}
+                            id='discordName'
+                            {...register('discordName', { required: true })}
+                            error={!!errors?.discordName}
                             sx={{ textAlign: 'center', paddingBottom: 5 }}
-                            >
+                        >
 
-                            </TextField>
-                            <br />
+                        </TextField>
+                        <br />
 
-                            <Box display='flex' justifyContent='center' flexDirection='column'  >
+                        <Box display='flex' justifyContent='center' flexDirection='column'  >
 
                             <Button
                                 variant='contained'
                                 type='submit'
                                 color='inherit'
-                                sx={{ borderRadius: 7, alignSelf:'center'}}
+                                sx={{ borderRadius: 7, alignSelf: 'center' }}
                             >
                                 Enviar
                             </Button>
 
 
-                            </Box>
+                        </Box>
 
-                         </form>
-                    
-                    </Card>
+                    </form>
 
-                
+                </Card>
+
+
             </Grid>
         </Box>
     )
--- src/pages/api/contestants/getInToContest.ts
+++ src/pages/api/contestants/getInToContest.ts
@@ -1,31 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = 
-| { message: string }
-| { resultado: string }
+type Data =
+    | { message: string }
+    | { resultado: string }
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
-    
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-    const { idUserDiscord, nameUserDiscord } = req.body;
+
+    const { discordId, discordName } = req.body;
     const url = 'https://devquest.azurewebsites.net/api/Contest/SaveUserContest';
 
     try {
         const connectToBackSaveUser = await fetch(url, {
             method: 'POST',
-            headers:{
+            headers: {
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify({ idUserDiscord, nameUserDiscord })
+            body: JSON.stringify({ discordId, discordName })
         })
 
         const data = await connectToBackSaveUser.json();
-        const {resultado} = data.Objeto.transaction;
-
-        
-        if( data.IsExito === false){
-            return res.status(412).json({ message: 'No posee acceso'})
-        }
+        const { resultado } = data.Objeto.transaction;
+        console.log(data)
 
         return res.status(200).json({
             ...data,
